09755711512073e6fe7ae6246dc3a7f8a57fc1e8
Paulo P <ct67ca@bosch.com>
2024-11-18 11:25:22 -0300

    18.11.2024

---
+++ html & css/database.js
@@ -0,0 +1,80 @@
+const database = [
+    {
+        codigoProduto: 1,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit com material leve e respirável, absorvendo o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product1.png",
+            img2: "./imagens/product2.png",
+            img3: "./imagens/product3.png",
+            img4: "./imagens/product4.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    
+    {
+        codigoProduto: 2,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit com material leve e respirável, absorvendo o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product1.png",
+            img2: "./imagens/product2.png",
+            img3: "./imagens/product3.png",
+            img4: "./imagens/product4.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 3,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit com material leve e respirável, absorvendo o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product1.png",
+            img2: "./imagens/product2.png",
+            img3: "./imagens/product3.png",
+            img4: "./imagens/product4.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 4,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit com material leve e respirável, absorvendo o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product1.png",
+            img2: "./imagens/product2.png",
+            img3: "./imagens/product3.png",
+            img4: "./imagens/product4.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto: 5,
+        tituloProduto: "Peças Dry Fit",
+        preco: 193.03,
+        descricao: "As Peças Dry Fit com material leve e respirável, absorvendo o suor de maneira eficiente.",
+        imagemProduto: {
+            img1: "./imagens/product1.png",
+            img2: "./imagens/product2.png",
+            img3: "./imagens/product3.png",
+            img4: "./imagens/product4.png"
+        },
+        categoriaProduto: "Roupas",
+        classificacaoProduto: "Novidades",
+        exibirHome: true
+    }
+];
+
+window.database = database;  // Tornando 'database' globalmente acessível
